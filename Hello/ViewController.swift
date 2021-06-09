a0bef49eeb0e7fcafd2d09c9fa8337952f97d15c
Kirill Ki <kirillkalapov@gmail.com>
2021-06-09 20:59:07 +0200

    In progress

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var dateTF: UITextField!
+    
+    @IBOutlet weak var monthTF: UITextField!
+    
+    
+    @IBOutlet weak var yearTF: UITextField!
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var findDayLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        findDayLabel.layer.cornerRadius = 7
     }
 
 
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        let calender = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTF.text!)
+        dateComponents.month = Int(monthTF.text!)
+        dateComponents.year = Int(yearTF.text!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let date = calender.date(from: dateComponents)
+        
+        let weekday = dateFormatter.string(from: date!)
+        
+        resultLabel.text = weekday
+        
+        
+        
+    }
 }
 
